9674ad053ec818e5f55c644d6dfe0e2065834fe9
Daniel <craklyn@gmail.com>
2026-08-10 22:44:55 -0700

    Implement 256-tile bitmask layout for flawless rendering parity

--- src/tilemap.lua
+++ src/tilemap.lua
@@ -68,23 +68,49 @@ function Tilemap:init()
     
     -- Create grass/dirt quads dynamically during draw using BITMASK_MAP
     self.BITMASK_MAP = {
-        [0] = {0,6}, [1] = {0,5}, [2] = {1,6}, [3] = {1,5},
-        [4] = {0,3}, [5] = {0,4}, [6] = {1,3}, [7] = {1,4},
-        [8] = {3,6}, [9] = {3,5}, [10] = {2,6}, [11] = {2,5},
-        [12] = {3,3}, [13] = {3,4}, [14] = {2,3}, [15] = {2,4}
+        [0] = {0, 6},   [1] = {0, 5},   [2] = {0, 6},   [3] = {0, 5},   [4] = {1, 6},   [5] = {0, 5},   [6] = {0, 6},   [7] = {0, 5},
+        [8] = {0, 6},   [9] = {0, 5},   [10] = {0, 6},  [11] = {0, 5},  [12] = {1, 6},  [13] = {0, 5},  [14] = {0, 6},  [15] = {0, 5},
+        [16] = {0, 3},  [17] = {0, 4},  [18] = {0, 3},  [19] = {0, 4},  [20] = {0, 3},  [21] = {0, 4},  [22] = {0, 3},  [23] = {0, 4},
+        [24] = {0, 3},  [25] = {0, 4},  [26] = {0, 3},  [27] = {0, 4},  [28] = {1, 6},  [29] = {0, 5},  [30] = {0, 6},  [31] = {0, 5},
+        [32] = {0, 6},  [33] = {0, 5},  [34] = {0, 6},  [35] = {0, 5},  [36] = {1, 6},  [37] = {0, 5},  [38] = {0, 6},  [39] = {0, 5},
+        [40] = {0, 6},  [41] = {0, 5},  [42] = {0, 6},  [43] = {0, 5},  [44] = {1, 6},  [45] = {0, 5},  [46] = {0, 6},  [47] = {0, 5},
+        [48] = {0, 3},  [49] = {0, 4},  [50] = {0, 3},  [51] = {0, 4},  [52] = {0, 3},  [53] = {0, 4},  [54] = {0, 3},  [55] = {0, 4},
+        [56] = {0, 6},  [57] = {0, 5},  [58] = {0, 6},  [59] = {0, 5},  [60] = {3, 3},  [61] = {0, 4},  [62] = {3, 3},  [63] = {0, 2},
+        [64] = {3, 6},  [65] = {0, 5},  [66] = {3, 6},  [67] = {0, 5},  [68] = {2, 6},  [69] = {2, 6},  [70] = {3, 6},  [71] = {0, 5},
+        [72] = {3, 6},  [73] = {0, 5},  [74] = {3, 6},  [75] = {0, 5},  [76] = {2, 6},  [77] = {2, 6},  [78] = {3, 6},  [79] = {2, 6},
+        [80] = {3, 6},  [81] = {0, 4},  [82] = {3, 6},  [83] = {0, 4},  [84] = {2, 6},  [85] = {1, 7},  [86] = {3, 6},  [87] = {1, 7},
+        [88] = {3, 6},  [89] = {0, 4},  [90] = {3, 6},  [91] = {0, 4},  [92] = {2, 6},  [93] = {1, 7},  [94] = {3, 6},  [95] = {1, 7},
+        [96] = {0, 6},  [97] = {0, 5},  [98] = {0, 6},  [99] = {0, 5},  [100] = {1, 6}, [101] = {0, 5}, [102] = {0, 6}, [103] = {0, 5},
+        [104] = {0, 6}, [105] = {0, 5}, [106] = {0, 6}, [107] = {0, 5}, [108] = {1, 6}, [109] = {0, 5}, [110] = {0, 6}, [111] = {0, 5},
+        [112] = {0, 3}, [113] = {0, 4}, [114] = {0, 3}, [115] = {0, 4}, [116] = {0, 3}, [117] = {0, 4}, [118] = {0, 3}, [119] = {0, 4},
+        [120] = {0, 6}, [121] = {0, 5}, [122] = {0, 6}, [123] = {0, 5}, [124] = {3, 3}, [125] = {0, 4}, [126] = {3, 3}, [127] = {2, 2},
+        [128] = {0, 6}, [129] = {0, 5}, [130] = {0, 6}, [131] = {0, 5}, [132] = {1, 6}, [133] = {0, 5}, [134] = {0, 6}, [135] = {0, 5},
+        [136] = {0, 6}, [137] = {0, 5}, [138] = {0, 6}, [139] = {0, 5}, [140] = {1, 6}, [141] = {0, 5}, [142] = {0, 6}, [143] = {0, 5},
+        [144] = {0, 3}, [145] = {0, 4}, [146] = {0, 3}, [147] = {0, 4}, [148] = {0, 3}, [149] = {0, 4}, [150] = {0, 3}, [151] = {0, 4},
+        [152] = {0, 3}, [153] = {0, 4}, [154] = {0, 3}, [155] = {0, 4}, [156] = {1, 6}, [157] = {0, 5}, [158] = {0, 6}, [159] = {0, 5},
+        [160] = {0, 6}, [161] = {0, 5}, [162] = {0, 6}, [163] = {0, 5}, [164] = {1, 6}, [165] = {0, 5}, [166] = {0, 6}, [167] = {0, 5},
+        [168] = {0, 6}, [169] = {0, 5}, [170] = {0, 6}, [171] = {0, 5}, [172] = {1, 6}, [173] = {0, 5}, [174] = {0, 6}, [175] = {0, 5},
+        [176] = {0, 3}, [177] = {0, 4}, [178] = {0, 3}, [179] = {0, 4}, [180] = {0, 3}, [181] = {0, 4}, [182] = {0, 3}, [183] = {0, 4},
+        [184] = {0, 6}, [185] = {0, 5}, [186] = {0, 6}, [187] = {0, 5}, [188] = {3, 3}, [189] = {0, 4}, [190] = {3, 3}, [191] = {1, 2},
+        [192] = {3, 6}, [193] = {0, 5}, [194] = {3, 6}, [195] = {0, 5}, [196] = {2, 6}, [197] = {2, 6}, [198] = {3, 6}, [199] = {0, 5},
+        [200] = {3, 6}, [201] = {0, 5}, [202] = {3, 6}, [203] = {0, 5}, [204] = {2, 6}, [205] = {2, 6}, [206] = {3, 6}, [207] = {2, 6},
+        [208] = {3, 6}, [209] = {0, 4}, [210] = {3, 6}, [211] = {0, 4}, [212] = {2, 6}, [213] = {1, 7}, [214] = {3, 6}, [215] = {1, 7},
+        [216] = {3, 6}, [217] = {0, 4}, [218] = {3, 6}, [219] = {0, 4}, [220] = {2, 6}, [221] = {1, 7}, [222] = {3, 6}, [223] = {1, 7},
+        [224] = {0, 6}, [225] = {0, 5}, [226] = {0, 6}, [227] = {0, 5}, [228] = {1, 6}, [229] = {0, 5}, [230] = {0, 6}, [231] = {0, 5},
+        [232] = {0, 6}, [233] = {0, 5}, [234] = {0, 6}, [235] = {0, 5}, [236] = {1, 6}, [237] = {0, 5}, [238] = {0, 6}, [239] = {0, 5},
+        [240] = {0, 3}, [241] = {0, 4}, [242] = {0, 3}, [243] = {0, 4}, [244] = {0, 3}, [245] = {0, 4}, [246] = {0, 3}, [247] = {0, 4},
+        [248] = {0, 6}, [249] = {0, 5}, [250] = {0, 6}, [251] = {0, 5}, [252] = {3, 3}, [253] = {0, 4}, [254] = {3, 3}, [255] = {2, 3}
     }
     
-    -- Pre-cache the 16 quads for grass and dirt
-    self.grassQuads = {}
+    -- Pre-cache the 256 quads for dirt
+    self.fullGrassQuad = love.graphics.newQuad(16, 16, TILE_SIZE, TILE_SIZE, self.tilesetImage:getDimensions())
     self.dirtQuads = {}
     self.wateredDirtQuads = {}
     
-    local tw, th = self.tilesetImage:getDimensions()
     local dw, dh = self.dirtImage:getDimensions()
-    for mask = 0, 15 do
+    for mask = 0, 255 do
         local cx = self.BITMASK_MAP[mask][1]
         local cy = self.BITMASK_MAP[mask][2]
-        self.grassQuads[mask] = love.graphics.newQuad(cx * TILE_SIZE, cy * TILE_SIZE, TILE_SIZE, TILE_SIZE, tw, th)
         self.dirtQuads[mask] = love.graphics.newQuad(cx * TILE_SIZE, cy * TILE_SIZE, TILE_SIZE, TILE_SIZE, dw, dh)
         self.wateredDirtQuads[mask] = love.graphics.newQuad((cx + 4) * TILE_SIZE, cy * TILE_SIZE, TILE_SIZE, TILE_SIZE, dw, dh)
     end
@@ -288,18 +314,13 @@ function Tilemap:drawBaseTiles()
             local py = (ty - 1) * TILE_SIZE
             
             -- Draw Grass background always
-            local grassMask = 0
-            if ty > 1 then grassMask = grassMask + 1 end
-            if tx < self.WIDTH then grassMask = grassMask + 2 end
-            if ty < self.HEIGHT then grassMask = grassMask + 4 end
-            if tx > 1 then grassMask = grassMask + 8 end
             -- Just draw full grass for the base
-            love.graphics.draw(self.tilesetImage, self.grassQuads[15], px, py)
+            love.graphics.draw(self.tilesetImage, self.fullGrassQuad, px, py)
             
             -- Draw Tilled Dirt if applicable
             local state = tile.state
             if state == "tilled" or state == "seeded" or state == "growing" or state == "ready" then
-                -- Calculate 4-way bitmask for dirt
+                -- Calculate 8-way bitmask for dirt
                 local mask = 0
                 local function isDirt(nx, ny)
                     local ntile = self:getTile(nx, ny)
@@ -307,10 +328,24 @@ function Tilemap:drawBaseTiles()
                     local s = ntile.state
                     return s == "tilled" or s == "seeded" or s == "growing" or s == "ready"
                 end
-                if ty > 1 and isDirt(tx, ty-1) then mask = mask + 1 end
-                if tx < self.WIDTH and isDirt(tx+1, ty) then mask = mask + 2 end
-                if ty < self.HEIGHT and isDirt(tx, ty+1) then mask = mask + 4 end
-                if tx > 1 and isDirt(tx-1, ty) then mask = mask + 8 end
+                
+                local cn = ty > 1 and isDirt(tx, ty-1)
+                local ce = tx < self.WIDTH and isDirt(tx+1, ty)
+                local cs = ty < self.HEIGHT and isDirt(tx, ty+1)
+                local cw = tx > 1 and isDirt(tx-1, ty)
+                local cne = ty > 1 and tx < self.WIDTH and isDirt(tx+1, ty-1)
+                local cse = ty < self.HEIGHT and tx < self.WIDTH and isDirt(tx+1, ty+1)
+                local csw = ty < self.HEIGHT and tx > 1 and isDirt(tx-1, ty+1)
+                local cnw = ty > 1 and tx > 1 and isDirt(tx-1, ty-1)
+
+                if cn then mask = mask + 1 end
+                if cn and ce and cne then mask = mask + 2 end
+                if ce then mask = mask + 4 end
+                if ce and cs and cse then mask = mask + 8 end
+                if cs then mask = mask + 16 end
+                if cs and cw and csw then mask = mask + 32 end
+                if cw then mask = mask + 64 end
+                if cw and cn and cnw then mask = mask + 128 end
                 
                 if tile.wateredToday then
                     love.graphics.draw(self.dirtImage, self.wateredDirtQuads[mask], px, py)
